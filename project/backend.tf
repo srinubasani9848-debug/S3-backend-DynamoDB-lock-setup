294abed77991698138b4effa0217cabc945cf0d0
srinubasani9848-debug <srinubasani9848@gmail.com>
2025-11-03 19:56:16 +0530

    Update backend.tf

--- project/backend.tf
+++ project/backend.tf
@@ -1,11 +1,9 @@
 terraform {
-  required_version = ">= 1.0.0"
-
   backend "s3" {
-    bucket         = "terraform-remote-state-srinivasbasani-12345"
-    key            = "demo/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks-srinivasbasani"
-    encrypt        = true
+    bucket       = "terraform-remote-state-srinivasbasani-12345"
+    key          = "demo/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
   }
 }
